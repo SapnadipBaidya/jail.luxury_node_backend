c875256e8e6fffbb57451fb20b8acc056092a45c
Sapnadip Baidya <sapnadipbaidya@192.168.0.105>
2025-01-19 01:11:26 +0530

    DEV SERVER CONN

--- config/connection.js
+++ config/connection.js
@@ -3,10 +3,8 @@ require("dotenv").config();
 const Sequelize = require("sequelize");
 
 // Allows for environmental variables to be used
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: "localhost",
+const sequelize =  new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: process.env.DB_IP,
       dialect: "mysql",
       dialectOptions: {
         decimalNumbers: true,
