74009962f56b0af283cd6db632e1b7aa1016832b
Sapnadip Baidya <sapnadipbaidya@192.168.1.2>
2025-03-23 23:29:20 +0530

    env change v1

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,8 @@ dotenv.config();
 // Allows for environmental variables to be used
 console.log("sqeee",process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW)
 const connection = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-  host: process.env.DB_IP,
+  host: process.env.DB_SOCKET_PATH,
+  port: process.env.DB_PORT,
   dialect: "mysql",
   dialectOptions: {
     decimalNumbers: true,
